d30ba5a367b96c72ae412ea4fbc42a50db358948
ouidad <ouidadelhirech2001@gmail.com>
2022-06-10 01:45:13 +0100

    initial commit

--- app/src/main/java/com/example/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/firebaselogin/MainActivity.java
@@ -1,31 +1,70 @@
 package com.example.firebaselogin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import javax.security.auth.login.LoginException;
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
     private Button btnLogout;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
+
+    @Override
+    public void onBackPressed() {
+        if(
+
+
+
+                drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        btnLogout = findViewById(R.id.btnlogout);
+        navigationView=findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_lay);
+        //  navigationView = findViewById(R.id.navigation_view);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+     /*       btnLogout = findViewById(R.id.btnlogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
                                          @Override
                                          public void onClick(View v) {
                                              logout();
                                          }
                                      }
-        );
+        );*/
+        mAuth = FirebaseAuth.getInstance();
     }
+
 
     @Override
     public void onStart() {
@@ -40,5 +79,13 @@ public class MainActivity extends AppCompatActivity {
     public void logout() {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout_btn: logout();
+        }
+        return true;
     }
 }
